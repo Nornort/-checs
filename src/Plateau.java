6120f7a2ed7330dfaa73bd6bd6ffffb149b1ea1a
francois <francois.lalleve@laposte.net>
2017-05-15 12:11:08 +0200

    Debug

--- src/Plateau.java
+++ src/Plateau.java
@@ -18,10 +18,10 @@ public class Plateau {
             for (int j = 0; j < 8; j += 7) {
                 plateau[i][j] = new Tour(couleur);
             }
-            for (int j = 1; j < 7; j += 6) {
+            for (int j = 1; j < 7; j += 5) {
                 plateau[i][j] = new Cavalier(couleur);
             }
-            for (int j = 2; j < 6; j += 5) {
+            for (int j = 2; j < 6; j += 3) {
                 plateau[i][j] = new Fou(couleur);
             }
             plateau[i][3 + k] = new Roi(couleur);
