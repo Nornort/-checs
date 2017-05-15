b3b5f25f8d27fee791bbdcb08554b8f2099df376
francois <francois.lalleve@laposte.net>
2017-05-15 03:09:58 +0200

    Début de classe Affichage

---
+++ src/Affichage.java
@@ -0,0 +1,12 @@
+/**
+ * Created by francois on 15/05/17.
+ */
+public class Affichage {
+    public static void afficher(Piece[][] tableau){
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                if(tableau[i][j]. == )
+            }
+        }
+    }
+}
