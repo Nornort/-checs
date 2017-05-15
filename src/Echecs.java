1faa5dc70b28e0b17d11d0fa2998e8c10105a040
francois <francois.lalleve@laposte.net>
2017-05-15 15:46:21 +0200

    A

--- src/Echecs.java
+++ src/Echecs.java
@@ -4,7 +4,9 @@
 import java.util.Scanner;
 public class Echecs {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         Plateau jeu = new Plateau();
         Affichage.afficher(jeu.plateau);
+        int str = sc.nextInt();
     }
 }
