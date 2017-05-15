bcf9985db6a93748905a0b0d6e827c135815f344
theoflament <theo.flament@insa-lyon.fr>
2017-05-15 11:00:47 +0200

    Update Affichage.java

--- src/Affichage.java
+++ src/Affichage.java
@@ -5,19 +5,22 @@ public class Affichage {
     public static void afficher(Piece[][] tableau){
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
-                if(tableau[i][j]. == 0 )
-                    System.out.println "Tour";
-                if(tableau[i][j]. == 1 )
-                    System.out.println "Cavalier";
-                if(tableau[i][j]. == 2 )
-                    System.out.println "Fou";
-                if(tableau[i][j]. == 3 )
-                    System.out.println "Reine";
-                if(tableau[i][j]. == 4 )
-                    System.out.println "Roi";
-                if(tableau[i][j]. == 5 )
-                    System.out.println "Pion";
+                if(tableau[i][j].type == 0 )
+                    System.out.println " ";
+                if(tableau[i][j].type == 1 )
+                    System.out.println "Ca";
+                if(tableau[i][j].type == 2 )
+                    System.out.println "Fo";
+                if(tableau[i][j].type == 3 )
+                    System.out.println "Re";
+                if(tableau[i][j].type == 4 )
+                    System.out.println "Ro";
+                if(tableau[i][j].type == 5 )
+                    System.out.println "Pi";
+                if(tableau[i][j].type == 6 )
+                    System.out.println "To";
             }
         }
+        System.out.println"|"
     }
 }
