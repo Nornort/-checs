243c5ed5f294ebf034191f61ffe5770a805127cc
francois <francois.lalleve@laposte.net>
2017-05-15 11:13:18 +0200

    Affichage

--- src/Plateau.java
+++ src/Plateau.java
@@ -3,22 +3,23 @@
  */
 public class Plateau {
     public Piece[][] plateau = new Piece[8][8];
-    public Plateau(){
-        for(int i=0;i < 8;i+=7){
+
+    public Plateau() {
+        for (int i = 0; i < 8; i += 7) {
             boolean couleur = false;
             int k = 0;
-            for(int j=0;j < 8;j+=7){
+            for (int j = 0; j < 8; j += 7) {
                 plateau[i][j] = new Tour(couleur);
             }
-            for(int j=1;j < 7;j+=6){
+            for (int j = 1; j < 7; j += 6) {
                 plateau[i][j] = new Cavalier(couleur);
             }
-            for(int j=2;j < 6;j+=5){
+            for (int j = 2; j < 6; j += 5) {
                 plateau[i][j] = new Fou(couleur);
             }
-            if(i == 7)k=1;
-            plateau[i][3+k] = new Roi(couleur);
-            plateau[i][4-k] = new Reine(couleur);
+            if (i == 7) k = 1;
+            plateau[i][3 + k] = new Roi(couleur);
+            plateau[i][4 - k] = new Reine(couleur);
             couleur = true;
             k++;
         }
