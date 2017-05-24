54b93be36c7bdec39ae1d43c7ef8ea623ade2e5e
maxvincent24 <maxime.vincent@philiaweb.com>
2017-05-24 08:50:41 +0200

    Update Tour.java

--- src/Tour.java
+++ src/Tour.java
@@ -5,4 +5,8 @@ public class Tour extends Piece{
     public Tour(boolean couleur){
         super(couleur,"To");
     }
+    public boolean deplacementPossible(int numeroPiece, String caseDeplacement){
+        String ligne = caseDeplacement[0];
+        int j = toInt(caseDeplacement[1]);
+    }
 }
