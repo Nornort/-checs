440da0db931ba37d92dc2bd2f59cbd55cc390750
maxvincent24 <maxime.vincent@philiaweb.com>
2017-05-24 09:04:15 +0200

    Update Tour.java

--- src/Tour.java
+++ src/Tour.java
@@ -2,9 +2,17 @@
  * Created by francois on 15/05/17.
  */
 public class Tour extends Piece{
+   
+    /** 
+	 * Constructeur de la classe tour
+	 * @param Prend en parametre la couleur de la tour
+	 * 
+	 */
     public Tour(boolean couleur){
         super(couleur,"To");
     }
+    
+    
     public boolean deplacementPossible(int numeroPiece, String caseDeplacement){
         String ligne = caseDeplacement[0];
         int j = toInt(caseDeplacement[1]);
