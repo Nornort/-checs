94e7e0ebcd32b55ddd05fcfb6fc1139815b7a7fa
theoflament <theo.flament@insa-lyon.fr>
2017-05-24 08:53:39 +0200

    Update Fou.java

--- src/Fou.java
+++ src/Fou.java
@@ -4,5 +4,34 @@
 public class Fou extends Piece{
     public Fou(boolean couleur){
         super(couleur,"Fo");
+           public boolean DeplacementPossible (int M; int N; int P; int Q)
+               int k;
+        boolean d;
+			if (P==M+k || Q==N+k){
+					tableau[M][N]=0;
+					tableau[P][Q]="Fo";
+            }    
+            if (P==M-k || Q==N+k){
+					tableau[M][N]=0;
+					tableau[P][Q]="Fo";
+            }
+            if (P==M+k || Q==N-k){
+					tableau[M][N]=0;
+					tableau[P][Q]="Fo";
+                }
+             if (P==M+k || Q==N+k){
+					tableau[M][N]=0;
+					tableau[P][Q]="Fo";
+             }
+             if (P==M+k || Q==N+k){
+					tableau[M][N]=0;
+					tableau[P][Q]="Fo";
+                
+			} else {
+					System.out.println("Mouvement impossible");
+				}
+			
+
+    
     }
 }
