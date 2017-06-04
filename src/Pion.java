e46f3605be578d18023b4a050d19d950bfc8c4eb
francois <francois.lalleve@laposte.net>
2017-06-04 19:29:48 +0200

    A

--- src/Pion.java
+++ src/Pion.java
@@ -3,7 +3,7 @@ public class Pion extends Piece {
     public Pion(boolean couleur) {super(couleur, "Pi");}
 
     public boolean deplacementPossible(Deplacement mouvement) {
-        if (mouvement.getdC() == 0){
+        if(mouvement.getdC() == 0){
             if(!this.couleur){
                 if(mouvement.getdL() <= (mouvement.getDepart().getLigne() == 1 ? 2 : 1) && mouvement.getdL() > 0) return true;
             }else if(mouvement.getdL() >= (mouvement.getDepart().getLigne() == 6 ? -2 : -1) && mouvement.getdL() < 0) return true;
@@ -14,10 +14,14 @@ public class Pion extends Piece {
         if(mouvement.getdL() == 0 && mouvement.getdC() == 0) return false;
         else if(!plateau[mouvement.getArrivee().getLigne()][mouvement.getArrivee().getColonne()].type.equals("  ") && this.couleur == plateau[mouvement.getArrivee().getLigne()][mouvement.getArrivee().getColonne()].couleur) return false;
         else if(this.deplacementPossible(mouvement)){
-            for(int i = 1; i < Math.abs(mouvement.getdL()); i++){
+            for(int i = 1; i <= Math.abs(mouvement.getdL()); i++){
                 if(!plateau[mouvement.getDepart().getLigne() + i*Integer.signum(mouvement.getdL())][mouvement.getDepart().getColonne()].type.equals("  ")) return false;
             }
             return true;
+        }else if(Math.abs(mouvement.getdL()) == 1 && Math.abs(mouvement.getdC()) == 1 && !plateau[mouvement.getArrivee().getLigne()][mouvement.getArrivee().getColonne()].type.equals("  ")){
+            if(!this.couleur){
+                if(mouvement.getdL() > 0) return true;
+            }else if(mouvement.getdL() < 0) return true;
         }
         return false;
     }
