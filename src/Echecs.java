0c9f82a0007122dda52f6f0b46c2ee887f44eb86
theoflament <theo.flament@insa-lyon.fr>
2017-06-04 20:13:27 +0200

    Update Echecs.java

--- src/Echecs.java
+++ src/Echecs.java
@@ -10,16 +10,19 @@ public class Echecs {
             couleur = !couleur;
             System.out.println("Au tour des " + (couleur ? "Blancs." : "Rouges."));
             String caseDepart = "0";
-            // Demande des coordonnées de départ et d'arrivée et réitère l'opération tant que le déplacement est impossible//
+            // Demande des coordonnées de départ //
             while(true){
                 do{
                     System.out.println("Quelle pièce voulez vous déplacer ?");
                     caseDepart = sc.nextLine();
+                    //Vérifie que la pièce choisi est du camp du joueur//
                     if (!jeu.pieceExiste(caseDepart,couleur)) System.out.println("Cette pièce n'est pas disponible");
+                //Demande les coordonnées d'arrivée//
                 }while(!jeu.pieceExiste(caseDepart,couleur));
                 String caseArrivee = "0";
                 System.out.println("Où cette pièce doit-elle aller ?");
                 caseArrivee = sc.nextLine();
+               //Vérifie que le déplacement est valide//
                 if (!jeu.deplacer(caseDepart, caseArrivee)) System.out.println("Ce déplacement est impossible !");
                 else break;
             }
